9d4218679f750db47764503802648805f3ebff46
pmetwp@l <pmetwp@l>
2017-01-19 20:50:06 +0900

    abc

--- WindowsApplication3/WindowsApplication3/frmgraph.Designer.vb
+++ WindowsApplication3/WindowsApplication3/frmgraph.Designer.vb
@@ -38,106 +38,117 @@ Partial Class Frmgraph
         'Panel1
         '
         Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.Panel1.Location = New System.Drawing.Point(118, 57)
+        Me.Panel1.Location = New System.Drawing.Point(88, 46)
+        Me.Panel1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(690, 122)
+        Me.Panel1.Size = New System.Drawing.Size(518, 98)
         Me.Panel1.TabIndex = 0
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(192, 39)
+        Me.Label1.Location = New System.Drawing.Point(144, 31)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(48, 15)
+        Me.Label1.Size = New System.Drawing.Size(38, 12)
         Me.Label1.TabIndex = 5
         Me.Label1.Text = "Label1"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(246, 39)
+        Me.Label2.Location = New System.Drawing.Point(184, 31)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(48, 15)
+        Me.Label2.Size = New System.Drawing.Size(38, 12)
         Me.Label2.TabIndex = 6
         Me.Label2.Text = "Label2"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(300, 39)
+        Me.Label3.Location = New System.Drawing.Point(236, 31)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(48, 15)
+        Me.Label3.Size = New System.Drawing.Size(38, 12)
         Me.Label3.TabIndex = 7
         Me.Label3.Text = "Label3"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(833, 57)
+        Me.Label4.Location = New System.Drawing.Point(625, 46)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(48, 15)
+        Me.Label4.Size = New System.Drawing.Size(38, 12)
         Me.Label4.TabIndex = 8
         Me.Label4.Text = "Label4"
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(836, 163)
+        Me.Label5.Location = New System.Drawing.Point(627, 130)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(48, 15)
+        Me.Label5.Size = New System.Drawing.Size(38, 12)
         Me.Label5.TabIndex = 9
         Me.Label5.Text = "Label5"
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(69, 56)
+        Me.Label6.Location = New System.Drawing.Point(52, 45)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(48, 15)
+        Me.Label6.Size = New System.Drawing.Size(38, 12)
         Me.Label6.TabIndex = 10
         Me.Label6.Text = "Label6"
         '
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(72, 162)
+        Me.Label7.Location = New System.Drawing.Point(54, 130)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(48, 15)
+        Me.Label7.Size = New System.Drawing.Size(38, 12)
         Me.Label7.TabIndex = 11
         Me.Label7.Text = "Label7"
         '
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(13, 56)
+        Me.Label8.Location = New System.Drawing.Point(10, 45)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(48, 15)
+        Me.Label8.Size = New System.Drawing.Size(38, 12)
         Me.Label8.TabIndex = 12
         Me.Label8.Text = "Label8"
         '
         'Label9
         '
         Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(16, 161)
+        Me.Label9.Location = New System.Drawing.Point(12, 129)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(48, 15)
+        Me.Label9.Size = New System.Drawing.Size(38, 12)
         Me.Label9.TabIndex = 13
         Me.Label9.Text = "Label9"
         '
         'Label10
         '
         Me.Label10.AutoSize = True
-        Me.Label10.Location = New System.Drawing.Point(580, 36)
+        Me.Label10.Location = New System.Drawing.Point(435, 29)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label10.Name = "Label10"
-        Me.Label10.Size = New System.Drawing.Size(56, 15)
+        Me.Label10.Size = New System.Drawing.Size(44, 12)
         Me.Label10.TabIndex = 14
         Me.Label10.Text = "Label10"
         '
         'Frmgraph
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(939, 418)
+        Me.ClientSize = New System.Drawing.Size(704, 334)
         Me.Controls.Add(Me.Label10)
         Me.Controls.Add(Me.Label9)
         Me.Controls.Add(Me.Label8)
@@ -149,6 +160,7 @@ Partial Class Frmgraph
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Panel1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Frmgraph"
         Me.Text = "Form2"
         Me.ResumeLayout(False)
