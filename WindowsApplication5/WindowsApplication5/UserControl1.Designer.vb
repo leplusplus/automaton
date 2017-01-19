60d721470da203ca22566c5a22a91a02f4a097a7
pmetwp@l <pmetwp@l>
2017-01-19 20:47:06 +0900

    abc

--- WindowsApplication5/WindowsApplication5/UserControl1.Designer.vb
+++ WindowsApplication5/WindowsApplication5/UserControl1.Designer.vb
@@ -22,17 +22,30 @@ Partial Class UserControl1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.UserControl21 = New WindowsApplication1.UserControl2()
         Me.SuspendLayout()
+        '
+        'UserControl21
+        '
+        Me.UserControl21.BackColor = System.Drawing.Color.Transparent
+        Me.UserControl21.incircle = False
+        Me.UserControl21.Location = New System.Drawing.Point(0, 0)
+        Me.UserControl21.Name = "UserControl21"
+        Me.UserControl21.Size = New System.Drawing.Size(315, 150)
+        Me.UserControl21.TabIndex = 0
+        Me.UserControl21.textw = Nothing
         '
         'UserControl1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Transparent
+        Me.Controls.Add(Me.UserControl21)
         Me.Name = "UserControl1"
         Me.Size = New System.Drawing.Size(259, 182)
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents UserControl21 As WindowsApplication1.UserControl2
 
 End Class
