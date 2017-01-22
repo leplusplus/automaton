4e26006ac8810ff6176a4ba742b274c73a28b0bb
pmetwp@l <pmetwp@l>
2017-01-22 12:34:17 +0900

    abc

---
+++ practice12/practice12/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Dim x(10000), y(10000), numberofPoint As Integer
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim i As Integer
+        numberofPoint = 0
+        For i = 0 To 9999
+            x(numberofPoint) = Rnd() * Panel1.Width
+            y(numberofPoint) = Rnd() * Panel1.Height
+
+            numberofPoint = numberofPoint + 1
+
+        Next
+        
+
+        Panel1.Refresh()
+
+    End Sub
+
+    Private Sub Panel1_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Panel1.Paint
+        Dim i As Integer
+        Dim g As Graphics
+        g = e.Graphics
+
+        For i = 0 To numberofPoint
+            g.DrawEllipse(Pens.Black, x(i) - 2, y(i) - 2, 4, 4)
+        Next
+    End Sub
+End Class
---
+++ practice12/practice12/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は以下の属性セットをとおして制御されます。 
+' アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更してください。
+
+' アセンブリ属性の値を確認します。
+
+<Assembly: AssemblyTitle("practice12")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("practice12")> 
+<Assembly: AssemblyCopyright("Copyright ©  2017")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります。
+<Assembly: Guid("69a5ea66-cd95-4aec-a49e-482cc1c84f1d")> 
+
+' アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+' 既定値にすることができます:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
